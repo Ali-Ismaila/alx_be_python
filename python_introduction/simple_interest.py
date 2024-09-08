2ed327804034fa048177e2717903057193090846
ali ismail <abdelwahab.a@kamc.med.sa>
2024-09-08 10:28:53 +0300

    simple_interestpy

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+#principal = 1000, rate = 0.05, and time = 3
+
+P = 1000
+R = 0.05
+T = 3
+
+
+#calculating simple interest
+SI = (P * R * T)
+#caculating amount = Simple Interest + Principal
+amount = SI
+#Printing the total amount
+print('The simple interest is:',amount)
