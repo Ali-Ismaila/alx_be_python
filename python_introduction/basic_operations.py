457f486a609347cd6c7562352137b333f544729e
ali ismail <abdelwahab.a@kamc.med.sa>
2024-09-08 10:05:33 +0300

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,22 @@
+##Addition of 10 and 5 is 15
+#Subtraction of 10 and 5 is 5
+#Multiplication of 10 and 5 is 50
+
+
+# This program adds two numbers
+
+number1 = 10
+number2 = 5
+
+# Add two numbers
+operation = number1 + number2
+operation1 = number1 - number2
+operation2 = number1 * number2
+
+# Display the Addition
+print('The Addition of {0} and {1} is {2}'.format(number1, number2, operation))
+
+
+print('The Subtraction of {0} and {1} is {2}'.format(number1, number2, operation1))
+
+print('The Multiplication of {0} and {1} is {2}'.format(number1, number2, operation2))
