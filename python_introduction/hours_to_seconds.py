f19a2a9b97b180be5f863f0195deb301ff632c8a
ali ismail <abdelwahab.a@kamc.med.sa>
2024-09-08 10:54:31 +0300

    hours_to_seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,8 +1,4 @@
-hours = 2
-seconds = hours * 60 * 60
-#print(seconds, " Seconds")
-
-print([hours], 'hour(s) is ',[seconds] ,'seconds.')
-
-#    Print the result in the format: [hours] hour(s) is [seconds] seconds.
-
+hours = 2 
+# Where 1hour =3600 
+seconds = hours * 3600
+print(hours, "hour(s) is", seconds, "seconds")
